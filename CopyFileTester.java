53123fe5323583fd585934c6562881ee34c3a724
Sydney <sassil1@hwemail.com>
2025-09-25 08:30:52 -0700

    Stretch Goal 2.4.2
    Advanced CleanUp Method works

--- CopyFileTester.java
+++ CopyFileTester.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,22 +33,22 @@ public class CopyFileTester {
         }
     }
 
-    public static void cleanUp(File f) throws IOException {
-        if (f.isDirectory()) {
-            for (File c : f.listFiles()) {
-                CopyFileTester.cleanUp(c);
-            }
-        }
-        if (!f.delete()) {
-            System.out.println("Failed to delete all files and folders");
-        } else {
-            System.out.println("Deleted " + f.getName());
-        }
-    }
+    // public static void cleanUp(File f) throws IOException {
+    //     if (f.isDirectory()) {
+    //         for (File c : f.listFiles()) {
+    //             CopyFileTester.cleanUp(c);
+    //         }
+    //     }
+    //     if (!f.delete()) {
+    //         System.out.println("Failed to delete all files and folders");
+    //     } else {
+    //         System.out.println("Deleted " + f.getName());
+    //     }
+    // }
 
     public static boolean isBlobInObjects(File f, boolean compression) throws NoSuchAlgorithmException, IOException {
         CopyFile gitproj = new CopyFile();
-        String directoryPath = "./git/objects"; 
+        String directoryPath = "./git/objects";
         File folder = new File(directoryPath);
         if (folder.exists() && folder.isDirectory()) {
             File[] files = folder.listFiles();
@@ -63,16 +64,48 @@ public class CopyFileTester {
         return false;
     }
 
+    public static void cleanUp() throws IOException {
+        File objects = new File("./git/objects");
+        if (!objects.exists()) {
+            objects.mkdir();
+        }
+        File[] files = objects.listFiles();
+        for (int i = files.length - 1; i >= 0; i--) {
+            files[i].delete();
+            System.out.println("Deleted " + files[i].getName());
+        }
+        File f = new File("output");
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+        f.delete();
+        File g = new File("output2");
+        if (!g.exists()) {
+            g.createNewFile();
+        }
+        g.delete();
+        File h = new File("output3");
+        if (!h.exists()) {
+            h.createNewFile();
+        }
+        h.delete();
+        String filePath = "./git/index";
+        FileWriter writer = new FileWriter(filePath, false);
+        writer.close();
+    }
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
+        // WARNING!! - if you change any of the file names assigned in here, the tester methods will no longer work properly!!!!
+        
         // initializing CopyFile Obj
         CopyFile gitproj = new CopyFile();
         gitproj.initializerepo();
 
-        //For Compression - to turn off compression, just set c = false
+        // For Compression - to turn off compression, just set c = false
         boolean c = false;
 
-        //Create and fills files for practicing with sha1 later
+        // Create and fills files for practicing with sha1 later
         File f = new File("output");
         if (!f.exists()) {
             f.createNewFile();
@@ -104,31 +137,23 @@ public class CopyFileTester {
         System.out.println(gitproj.genSha1(f, c));
 
         // Takes 3 files, turns into sha1 blobs, stores in index and creates files in objects with the sha1 hash as their names
-        gitproj.storeFileObj(f,c);
-        gitproj.storeFileObj(g,c);
-        gitproj.storeFileObj(h,c);
-        gitproj.storeFileInd(f,c);
-        gitproj.storeFileInd(g,c);
-        gitproj.storeFileInd(h,c);
+        gitproj.storeFileObj(f, c);
+        gitproj.storeFileObj(g, c);
+        gitproj.storeFileObj(h, c);
+        gitproj.storeFileInd(f, c);
+        gitproj.storeFileInd(g, c);
+        gitproj.storeFileInd(h, c);
 
-        //Making sure blobs are in objects (Stretch Goal 2.3.1)
-        System.out.println(isBlobInObjects(f,c));
-        System.out.println(isBlobInObjects(g,c));
-        System.out.println(isBlobInObjects(h,c));
+        // Making sure blobs are in objects (Stretch Goal 2.3.1)
+        System.out.println(isBlobInObjects(f, c));
+        System.out.println(isBlobInObjects(g, c));
+        System.out.println(isBlobInObjects(h, c));
 
-        // Reseting (Stretch Goal 2.3.1) (Comment out for testing the above, then
-        // uncomment and run before testing again)
-        File git = new File("./git");
-        cleanUp(git);
-        File i = gitproj.zipCompress(f);
-        File j = gitproj.zipCompress(g);
-        File k = gitproj.zipCompress(h);
-        i.delete();
-        j.delete();
-        k.delete();
-        f.delete();
-        g.delete();
-        h.delete();
+        // Reseting (Stretch Goal 2.3.1--> 2.4.2) (Comment out for testing the above, then uncomment and run before testing again)
+        cleanUp();
+
+        //TO-DO: MAKE SURE ZIP COMPRESSION IS CORRECT AND MAKE SURE HEAD IS CORRECT
+
 
     }
 }
