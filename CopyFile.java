38f7a57a9dae134802633560453dda95393d424a
jreiner16 <joereiner16@gmail.com>
2025-09-30 13:46:59 -0700

    Consolidated Imports

--- CopyFile.java
+++ CopyFile.java
@@ -1,20 +1,8 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.nio.file.*;
+import java.security.*;
+import java.util.zip.*;
 
 public class CopyFile {
 
--- CopyFileTester.java
+++ CopyFileTester.java
@@ -1,9 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.*;
+import java.nio.file.*;
 import java.security.NoSuchAlgorithmException;
 
 public class CopyFileTester {
